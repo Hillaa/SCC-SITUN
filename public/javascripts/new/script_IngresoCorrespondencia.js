19310100641e3e10cbd97d3770dde8efc61ee7db
JOSE AVALOS <nachoab4@hotmail.com>
2017-03-22 15:04:34 -0600

    Quita mansajes al cancelar

--- public/javascripts/new/script_IngresoCorrespondencia.js
+++ public/javascripts/new/script_IngresoCorrespondencia.js
@@ -250,6 +250,8 @@ function validacionCampos(){	 //Valida todos los campos
 function botonCancelar(){//Metodo de cancelar
 	limpiarCampos();
 	limpiaDivMensaje();
+	  //$("[data-toggle='tooltip']")removeAttr("data-original-title");
+	
 	subePagina();//sube la pagina
 }
 function limpiarCampos(){//Limpia el valor de los campos de entrada
@@ -277,6 +279,15 @@ function limpiarCampos(){//Limpia el valor de los campos de entrada
 	}
 function limpiaDivMensaje(){//Limpia el div con el id=mensaje
 	$("#mensaje").text("");
+    $("#IC1").removeAttr("data-original-title");
+	$("#IC2").removeAttr("data-original-title");
+	$("#IC3").removeAttr("data-original-title");
+	$("#IC4").removeAttr("data-original-title");
+	$("#IC5").removeAttr("data-original-title");
+	$("#IC6").removeAttr("data-original-title");
+	$("#IC7").removeAttr("data-original-title");
+	$("#IC8").removeAttr("data-original-title");
+	$("#IC9").removeAttr("data-original-title");
 }
 function cambioClase1(op){//Realiza un cambio de clase a los campos de entrada del formulario de la clase has-error a from-group
 	switch(op){
